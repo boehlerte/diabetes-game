d49d72eaf210e31f36edc35ef24f627909faae3d
Erin <boehlert@email.unc.edu>
2017-03-07 20:58:22 -0500

    Add files via upload
    added contact detection and fixed bug -- multiple foods of the same type can be on the screen at the same time

--- GameScene.swift
+++ GameScene.swift
@@ -1,19 +1,36 @@
 import SpriteKit
 
+enum object:UInt32{
+    case food = 1
+    case player = 2
+}
 
-
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
+    
+   
     
     var collection = [Foods]()
-
+    
     
     let player = SKSpriteNode(imageNamed: "dot")
-    var touched:Bool = false
-    var location = CGPoint(x: 0, y: 0)
+    
+    var playerTouched:Bool = false
+    var playerLocation = CGPoint(x: 0, y: 0)
     
     override func didMove(to view: SKView) {
+        
+        physicsWorld.contactDelegate = self
+        
         backgroundColor = SKColor.cyan
         player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
+        player.physicsBody?.affectedByGravity = false
+        player.physicsBody?.categoryBitMask = object.player.rawValue
+        player.physicsBody?.collisionBitMask = 0
+        player.physicsBody?.contactTestBitMask = object.food.rawValue
+        
+        player.name = "player"
         addChild(player)
         
         NewFood()
@@ -22,31 +39,31 @@ class GameScene: SKScene {
         run(SKAction.repeatForever(
             SKAction.sequence([
                 SKAction.run(addFood),
-                SKAction.wait(forDuration: 4.0)
+                SKAction.wait(forDuration: 1.0)
                 ])
         ))
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
-        touched = true
+        playerTouched = true
         for touch in touches {
-            location = touch.location(in: self)
+            playerLocation = touch.location(in: self)
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?){
         for touch in touches {
-            location = touch.location(in: self)
+            playerLocation = touch.location(in: self)
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?){
-        touched = false
+        playerTouched = false
     }
     
     override func update(_ currentTime: CFTimeInterval) {
-        if(touched) {
+        if(playerTouched) {
             moveNodeToLocation()
         }
     }
@@ -54,8 +71,8 @@ class GameScene: SKScene {
     func moveNodeToLocation() {
         let speed: CGFloat = 0.25
         
-        var dx = location.x - player.position.x
-        var dy = location.y - player.position.y
+        var dx = playerLocation.x - player.position.x
+        var dy = playerLocation.y - player.position.y
         
         dx = dx * speed
         dy = dy * speed
@@ -63,7 +80,7 @@ class GameScene: SKScene {
         
     }
     
-
+    
     
     func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
@@ -76,32 +93,37 @@ class GameScene: SKScene {
     func addFood() {
         
         
-      //  let apple = SKSpriteNode(imageNamed: "apple")
+        //  let apple = SKSpriteNode(imageNamed: "apple")
         
         //////
         let randd = Int(arc4random_uniform(3))
         // random number casted as int
-        let apple = collection[randd].foodType
+        let food = collection[randd].foodType.copy() as! SKSpriteNode
         //////
         
         // Determine where to spawn the food along the Y axis
-        let actualY = random(min: apple.size.height/2, max: size.height - apple.size.height/2)
+        let actualY = random(min: food.size.height/2, max: size.height - food.size.height/2)
         
-        apple.position = CGPoint(x: size.width + apple.size.width/2, y: actualY)
+        food.position = CGPoint(x: size.width + food.size.width/2, y: actualY)
+        food.physicsBody = SKPhysicsBody(circleOfRadius: food.size.width/2)
+        food.physicsBody?.affectedByGravity = false
+        food.physicsBody?.collisionBitMask = 0
+        food.physicsBody?.categoryBitMask = object.food.rawValue
+        food.name = "food"
+        
         
         // Add the food to the game
-        addChild(apple)
+        addChild(food)
         
         // Calculate the speed of the food
         let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
         
-        let actionMove = SKAction.move(to: CGPoint(x: -apple.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
+        let actionMove = SKAction.move(to: CGPoint(x: -food.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
         let actionMoveDone = SKAction.removeFromParent()
-        apple.run(SKAction.sequence([actionMove, actionMoveDone]))
+        food.run(SKAction.sequence([actionMove, actionMoveDone]))
         
     }
-    
-    
+
     
     func NewFood() {
         
@@ -120,10 +142,21 @@ class GameScene: SKScene {
         let Strawberry = SKSpriteNode(imageNamed: "Foods.sprite/Strawberry.png")
         let f4 = Foods(carb_count: 5, carb: true, foodType: Strawberry)
         collection.append(f4)
-        
-        
-        
+
         
     }
     
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if(contact.bodyA.node?.name == "food"){
+            contact.bodyA.node?.removeFromParent()
+        }else if(contact.bodyB.node?.name == "food"){
+            contact.bodyB.node?.removeFromParent()
+        }
+        
+        
+    }
+   
+        
 }
