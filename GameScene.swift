2e6e40eb08d4bbbc2f585a24db2fb42708d72b1d
Erin <boehlert@email.unc.edu>
2017-03-14 16:06:15 -0400

    minimal meter functionality

--- GameScene.swift
+++ GameScene.swift
@@ -1,4 +1,3 @@
-
 import SpriteKit
 
 enum object:UInt32{
@@ -8,26 +7,26 @@ enum object:UInt32{
 
 class GameScene: SKScene, SKPhysicsContactDelegate{
     
-    var collection = [Foods]()
-    // create array for all food items
+   
     
-    let player = SKSpriteNode(imageNamed: "ram")
+    var collection = [Foods]()
+    
+    let player = SKSpriteNode(imageNamed: "dot")
     
     var playerTouched:Bool = false
     var playerLocation = CGPoint(x: 0, y: 0)
     
-   // use this for sound effects, then call playSound(sound: sound) in DidMove
-   // var sound = SKAction.playSoundFileNamed("sound.wav", waitForCompletion: false)
+    
+    var streak = 0
+    var foodMeter = SKSpriteNode(color: SKColor .yellow, size: CGSize(width: 0, height: 50))
+    
     
     override func didMove(to view: SKView) {
-        
-        let backgroundMusic = SKAudioNode(fileNamed: "BackgroundMusic.wav")
-        self.addChild(backgroundMusic)
         
         physicsWorld.contactDelegate = self
         
         backgroundColor = SKColor.cyan
-        player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.25)
+        player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
         
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
         player.physicsBody?.affectedByGravity = false
@@ -41,7 +40,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         NewFood()
         // create all the foods and put them in an array
         
-       
         run(SKAction.repeatForever(
             SKAction.sequence([
                 SKAction.run(addFood),
@@ -49,9 +47,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
                 ])
         ))
         
+        //clear out section at top of screen for meter
+        let meterFrame = SKShapeNode(rectOf: CGSize(width: size.width * 2, height: 100))
+        meterFrame.fillColor = SKColor .white
+        meterFrame.position = CGPoint(x: size.width, y: 50)
+        addChild(meterFrame)
+        
+        //add fill of meter as food is collected
+       
+        foodMeter.name = "meter"
+        foodMeter.position = CGPoint(x: 0 , y: 50)
+        foodMeter.anchorPoint = CGPoint(x: 0.0, y: 0.5)
+        
+        addChild(foodMeter)
+        
     }
-    
-    // RECOGNIZING TOUCH GESTURES
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         playerTouched = true
@@ -100,12 +110,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     
     func addFood() {
         
+        
+        //  let apple = SKSpriteNode(imageNamed: "apple")
+        
+        //////
         let randd = Int(arc4random_uniform(3))
-        // random number casted as int to pick food to show next
+        // random number casted as int
         let food = collection[randd].foodType.copy() as! SKSpriteNode
+        //////
         
         // Determine where to spawn the food along the Y axis
-        let actualY = random(min: food.size.height/2, max: (size.height - food.size.height/2)/2)
+        let actualY = random(min: food.size.height/2, max: size.height - food.size.height/2)
         
         food.position = CGPoint(x: size.width + food.size.width/2, y: actualY)
         food.physicsBody = SKPhysicsBody(circleOfRadius: food.size.width/2)
@@ -126,8 +141,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         food.run(SKAction.sequence([actionMove, actionMoveDone]))
         
     }
+
     
-    //CREATE NEW FOODS FOR GAME
     func NewFood() {
         
         let Bread = SKSpriteNode(imageNamed: "Foods.sprite/Bread.png")
@@ -145,24 +160,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         let Strawberry = SKSpriteNode(imageNamed: "Foods.sprite/Strawberry.png")
         let f4 = Foods(carb_count: 5, carb: true, foodType: Strawberry)
         collection.append(f4)
-        
+
         
     }
     
-    // COLLISION DETECTION
+    
     func didBegin(_ contact: SKPhysicsContact) {
         
         if(contact.bodyA.node?.name == "food"){
             contact.bodyA.node?.removeFromParent()
+            streak += 1
+            incrementMeter()
         }else if(contact.bodyB.node?.name == "food"){
             contact.bodyB.node?.removeFromParent()
+            streak += 1
+            incrementMeter()
         }
+        
         
     }
     
-    // CALL THIS TO PLAY MUSIC/SOUND
-    func playSound(sound : SKAction) {
-        run(sound)
+    //increment meter by number of carbs
+    func incrementMeter(){
+        foodMeter.size = CGSize(width: foodMeter.size.width + 30, height: foodMeter.size.height)
     }
-
+   
+        
 }
